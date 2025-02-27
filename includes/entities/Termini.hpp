64e7d271a5eda510e75da4bf2f76fd5e68458e16
milmip <emilien.progin@gmail.com>
2025-02-27 09:15:48 +0100

    First termini module working

--- includes/entities/Termini.hpp
+++ includes/entities/Termini.hpp
@@ -1,9 +1,23 @@
 #ifndef TERMINI
 #define TERMINI
 
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 //#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include <iostream>
+#include <map>
+#include <string>
+
+struct Character {
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2   Size;      // Size of glyph
+    glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Horizontal offset to advance to next glyph
+};
 
 class Termini
 {
@@ -16,7 +30,11 @@ public:
 
 private:
 	unsigned int shader;
-	glm::mat4 projMat = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
-	
+
+	std::map<GLchar, Character> Characters;
+	unsigned int VAO, VBO;
+	glm::mat4 glyphProjMatrix = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+
+	void RenderText(unsigned int SHADERR, std::string text, float x, float y, float scale, glm::vec3 color);	
 };
 #endif
